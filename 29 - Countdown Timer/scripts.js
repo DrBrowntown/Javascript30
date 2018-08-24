49f9aaae4ed1c5485c103f160fe715fccc2109a8
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-08-24 16:57:22 -0400

    feat: Display return hour in 12 hour format

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -6,6 +6,7 @@ function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
+    displayEndTime(then);
 
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -32,5 +33,6 @@ function displayTimeLeft(seconds) {
 function displayEndTime(timestamp) {
     const end = new Date(timestamp);
     const hour = end.getHours();
-    const minutes = end.getMinutes
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${hour > 12 ? hour - 12: hour}:${minutes}`;
 }
