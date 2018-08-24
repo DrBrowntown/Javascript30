9cb9b8278b19e949d343e98c2eee8b75ebcf48fd
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-08-24 16:34:07 -0400

    feat: Define setInterval() to console log the count down in seconds entered into the timer() function

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -1,5 +1,9 @@
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
+    setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        console.log(secondsLeft);
+    }, 1000);
     console.log({now, then});
 }
