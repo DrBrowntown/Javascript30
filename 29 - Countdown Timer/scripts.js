28e2066847eabd30c269776974088b55d5b08c5f
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-08-24 17:03:00 -0400

    feat: Tie timer() to startTimer() buttons

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -39,7 +39,8 @@ function displayEndTime(timestamp) {
 }
 
 function startTimer() {
-    console.log(this);
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
 }
  
 buttons.forEach(button => button.addEventListener('click', startTimer));
